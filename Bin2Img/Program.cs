bc28b915ffebff82a830e00d11ad4515ce812f4a
mowei <mowei.tw@gmail.com>
2018-10-25 23:24:30 +0800

    Init

---
+++ Bin2Img/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Bin2Img
+{
+    class Program
+    {
+        static readonly string savePath = @"C:\temp\";
+        static readonly string srcPath = @"C:\temp\src.bin";
+        static readonly int width = 1600;
+        static readonly int height = 800;
+        static readonly int latticeSize = 1;//min = 1
+        static readonly int interval_width = 0;
+        static readonly int interval_height = 16;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var fileByteArrary = FileToByteArrary(srcPath);
+            if (fileByteArrary.Length == 0)
+            {
+                return;
+            }
+            ByteArrayToBitmap(fileByteArrary);
+
+        }
+        public static byte[] FileToByteArrary(String pathSource)
+        {
+            try
+            {
+                using (FileStream fsSource = new FileStream(pathSource, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] bytes = new byte[fsSource.Length + 4];
+                    int numBytesToRead = (int)fsSource.Length;
+                    int numBytesRead = 0;
+                    Buffer.BlockCopy(BitConverter.GetBytes(numBytesToRead), 0, bytes, 0, 4);
+                    numBytesRead = 4;
+                    while (numBytesToRead > 0)
+                    {
+                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+                        if (n == 0)
+                            break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+                    numBytesToRead = bytes.Length;
+                    return bytes;
+                }
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+                return new byte[0];
+            }
+        }
+        public static void ByteArrayToBitmap(byte[] byteArray)
+        {
+
+            var fileNumBytesRead = byteArray.Length;
+            var fileNumBytesToRead = 0;
+
+            int part = 0;
+            while (fileNumBytesToRead < fileNumBytesRead)
+            {
+                Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+                for (var i = 0; i < height; i += (latticeSize + interval_width))
+                {
+                    for (var j = 0; j < width; j += latticeSize)
+                    {
+                        var r = 0;
+                        if (fileNumBytesToRead < fileNumBytesRead)
+                        {
+                            r = byteArray[fileNumBytesToRead];
+                            fileNumBytesToRead++;
+                        }
+                        var g = 0;
+                        if (fileNumBytesToRead < fileNumBytesRead)
+                        {
+                            g = byteArray[fileNumBytesToRead];
+                            fileNumBytesToRead++;
+                        }
+                        var b = 0;
+                        if (fileNumBytesToRead < fileNumBytesRead)
+                        {
+                            b = byteArray[fileNumBytesToRead];
+                            fileNumBytesToRead++;
+                        }
+
+                        for (var k = 0; k < latticeSize; k++)
+                        {
+                            for (var l = 0; l < latticeSize; l++)
+                            {
+                                bmp.SetPixel(j + l, i + k, Color.FromArgb(r, g, b));
+                            }
+                        }
+                    }
+                    if ((i + 1) % interval_height == 0)
+                    {
+                        i += interval_height;
+                    }
+                }
+                Directory.CreateDirectory(savePath);
+                bmp.Save($@"{savePath}Img{part}.bmp", ImageFormat.Png);
+                part++;
+                bmp.Dispose();
+            }
+        }
+    }
+}
---
+++ Img2bin/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Img2Bin
+{
+    class Program
+    {
+        static readonly string outPath = @"C:\temp\Save.bin";
+        static readonly string outTempPath = @"C:\temp\Temp";
+        static readonly int frameWidth = 1600;
+        static readonly int frameHeight = 800;
+        static readonly int latticeSize = 1;//min = 1
+        static readonly int interval_width = 0;
+        static readonly int interval_height = 16;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var fileSize = 0;
+            int numBytesWrite = 0;
+
+            using (FileStream fsSource = new FileStream(outPath, FileMode.Append, FileAccess.Write))
+            {
+                for (var i = 0; i < 50; i++)
+                {
+                    string srcPath = $@"C:\temp\Img{i}.bmp";
+                    try
+                    {
+                        var data = Img2bin(srcPath);
+                        var dataLength = data.Length;
+                        var offset = 0;
+                        if (i == 0)
+                        {
+                            var temp = new byte[4];
+                            Buffer.BlockCopy(data, 0, temp, 0, 4);
+                            //Array.Reverse(temp);
+                            fileSize = BitConverter.ToInt32(temp, 0);
+                            dataLength -= 4;
+                            offset += 4;
+                        }
+
+                        if (numBytesWrite + dataLength > fileSize)
+                        {
+                            fsSource.Write(data, offset, fileSize - numBytesWrite);
+                            numBytesWrite += fileSize - numBytesWrite;
+                        }
+                        else
+                        {
+                            fsSource.Write(data, offset, dataLength);
+                            numBytesWrite += dataLength;
+                        }
+                    }
+
+                    catch (Exception ex)
+                    {
+                        return;
+                    }
+                }
+            }
+
+        }
+        public static byte[] Img2bin(String srcPath)
+        {
+            int offsetX = 0;
+            int offsetY = 0;
+            var fileNumBytesToRead = 0;
+            Bitmap bmp = new Bitmap(srcPath);
+            //Bitmap getBmp = new Bitmap(frameWidth, frameHeight);
+            byte[] byteArray = new byte[frameWidth * frameHeight * 3];
+            for (var i = 0; i < frameHeight; i += (latticeSize + interval_width))
+            {
+                for (var j = 0; j < frameWidth; j += latticeSize)
+                {
+                    int colorR = 0;
+                    int colorG = 0;
+                    int colorB = 0;
+                    for (var k = 0; k < latticeSize; k++)
+                    {
+                        for (var l = 0; l < latticeSize; l++)
+                        {
+                            var color = bmp.GetPixel(offsetX + j + l, offsetY + i + k);
+                            //getBmp.SetPixel(j,i,color);
+                            colorR += color.R;
+                            colorG += color.G;
+                            colorB += color.B;
+                        }
+                    }
+                    byteArray[fileNumBytesToRead++] = (byte)(colorR / (latticeSize * latticeSize));
+                    byteArray[fileNumBytesToRead++] = (byte)(colorG / (latticeSize * latticeSize));
+                    byteArray[fileNumBytesToRead++] = (byte)(colorB / (latticeSize * latticeSize));
+                }
+                if ((i + 1) % interval_height == 0)
+                {
+                    i += interval_height;
+                }
+            }
+            //getBmp.Save(srcPath + ".temp.png");
+            byte[] resultBytes = new byte[fileNumBytesToRead];
+            resultBytes = byteArray.Take(fileNumBytesToRead).ToArray();
+            return resultBytes;
+        }
+    }
+}
